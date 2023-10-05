bc0c53d3415642f3e3a7e562d2068ea51253b03b
Gavin Uberti <gavin.uberti@gmail.com>
2023-10-05 14:56:23 -0700

    chat intros

---
+++ src/lib/components/chat/APIChatIntroduction.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { PUBLIC_APP_NAME, PUBLIC_VERSION } from "$env/static/public";
+	import { PUBLIC_ANNOUNCEMENT_BANNERS } from "$env/static/public";
+	import Logo from "$lib/components/icons/Logo.svelte";
+	import { createEventDispatcher } from "svelte";
+	import IconChevron from "$lib/components/icons/IconChevron.svelte";
+	import CarbonArrowUpRight from "~icons/carbon/arrow-up-right";
+	import AnnouncementBanner from "../AnnouncementBanner.svelte";
+	import ModelsModal from "../ModelsModal.svelte";
+	import type { Model } from "$lib/types/Model";
+	import ModelCardMetadata from "../ModelCardMetadata.svelte";
+	import type { LayoutData } from "../../../routes/$types";
+	import { findCurrentModel } from "$lib/utils/models";
+
+	export let settings: LayoutData["settings"];
+
+	let isModelsModalOpen = false;
+
+	const announcementBanners = PUBLIC_ANNOUNCEMENT_BANNERS
+		? JSON.parse(PUBLIC_ANNOUNCEMENT_BANNERS)
+		: [];
+
+	const dispatch = createEventDispatcher<{ message: string }>();
+</script>
+
+<div class="my-auto grid gap-8 lg:grid-cols-3">
+	<div class="lg:col-span-1">
+		<div>
+			<div class="mb-3 flex items-center text-2xl font-semibold">
+				<Logo classNames="mr-1 flex-none" />
+				Etched []
+				<div
+					class="ml-3 flex h-6 items-center rounded-lg border border-gray-100 bg-gray-50 px-2 text-base text-gray-400 dark:border-gray-700/60 dark:bg-gray-800"
+				>
+					v{PUBLIC_VERSION}
+				</div>
+			</div>
+			<p class="text-base text-gray-600 dark:text-gray-400">
+				We build specialized microchips, just for Transformer models. Our chips aren't ready yet, but [add more copy]. This should be a lot of explaining
+			</p>
+		</div>
+	</div>
+	<div class="lg:col-span-2 lg:pl-24">
+		<div class="overflow-hidden rounded-xl border dark:border-gray-800">
+			<div class="flex p-3">
+				<div class="flex flex-col justify-center mr-4"> <!-- Add margin for spacing -->
+					<div class="text-sm text-gray-600 dark:text-gray-400 flex items-center"> <!-- Added flex here -->
+						<span>Current Model: </span> <!-- Wrapped text in span for better control -->
+						<span class="font-semibold text-black ml-2">Llama-2-70b-chat</span> <!-- Added margin for spacing -->
+					</div>
+					<span class="text-sm text-gray-600 dark:text-gray-400">140.2 GB, running dense float16 precision</span>
+				</div>
+			</div>
+		</div>
+	</div>
+<!--	{#if currentModelMetadata.promptExamples}
+		<div class="lg:col-span-3 lg:mt-6">
+			<p class="mb-3 text-gray-600 dark:text-gray-300">Examples</p>
+			<div class="grid gap-3 lg:grid-cols-3 lg:gap-5">
+				{#each currentModelMetadata.promptExamples as example}
+					<button
+						type="button"
+						class="rounded-xl border bg-gray-50 p-2.5 text-gray-600 hover:bg-gray-100 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 sm:p-4"
+						on:click={() => dispatch("message", example.prompt)}
+					>
+						{example.title}
+					</button>
+				{/each}
+			</div>
+		</div>{/if}-->
+</div>
